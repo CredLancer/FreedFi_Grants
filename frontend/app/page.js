d59dc6aa8d4213b395fc701a462bdd9a835f8bbd
Balaganesh K <k.balaganesh26@gmail.com>
2024-01-06 16:48:25 +0530

    Updated the home page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,9 @@
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
-    </main>
+    <div>Home</div>
   )
 }
+
+export default Home
