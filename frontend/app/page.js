699a6736fae52a9e0298e779e51973862892a490
Balaganesh K <k.balaganesh26@gmail.com>
2024-01-06 18:07:16 +0530

    added the theme provider

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,14 @@
+import ThemeSwitch from '@/components/ThemeSwitch'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen min-w-screen dark:bg-gray-800 bg-gray-100 text-gray-800 dark:text-gray-100'>
+      <ThemeSwitch />
+      <h1>
+      Home
+      </h1>
+    </div>
   )
 }
 
