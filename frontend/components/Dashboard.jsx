19cec01986d9d05d5e75ddae5aa7f8518d29d448
Balaganesh K <k.balaganesh26@gmail.com>
2024-01-07 19:27:01 +0530

    updated the dashboard trust credit score

--- frontend/components/Dashboard.jsx
+++ frontend/components/Dashboard.jsx
@@ -14,9 +14,9 @@ const Dashboard = () => {
     <main className="px-6 md:px-10 flex flex-col gap-10 h-full pb-16">
       <div className=" w-full mt-[8rem] h-full sm:h-96 gap-5 flex flex-col lg:flex-row ">
         <section className="w-full basis-[50%] flex-1 sm:h-96 px-6 py-10 bg-zinc-900 rounded-3xl flex-col justify-center items-center gap-10 flex">
-          <header className="flex-col justify-center items-center gap-2 flex">
+          <header className="flex-col  items-center gap-2 flex">
             <h1
-              className={`${prostoOne.className} text-purple-500 text-[28px] font-normal leading-10`}>
+              className={`${prostoOne.className} text-purple-500  text-[28px] font-normal leading-10`}>
               Your skill match score
             </h1>
             <div className="w-[200px] h-[203.38px] relative">
@@ -58,23 +58,22 @@ const Dashboard = () => {
           />
         </aside>
       </div>
-      <div className="w-full min-h-[400px] h-full p-4 md:p-10 bg-zinc-900 rounded-3xl flex flex-col lg:flex-row justify-start items-center gap-10">
-        <div className="w-full md:basis-[50%] p-5 bg-neutral-800 rounded-2xl flex flex-col justify-center items-center gap-10">
+      <section className="w-full min-h-[400px] h-full p-4 md:p-10 bg-zinc-900 rounded-3xl flex flex-col lg:flex-row justify-start items-center gap-10">
+        <div className="w-full md:basis-[50%] p-5 bg-neutral-800 rounded-2xl flex flex-col justify-center  gap-10">
           <div
-            className={`text-lime-400 text-lg md:text-[28px] font-normal ${prostoOne.className} leading-10`}>
+            className={`text-lime-400 text-[28px] font-normal ${prostoOne.className} leading-10`}>
             Trust credit score
           </div>
-          <div className="w-60 h-[200px] relative">
+          <div className="w-60 h-[200px] relative mx-auto">
             <div
-              className={`text-center text-white text-lg md:text-[26.46px] font-normal ${prostoOne.className} leading-[30.07px]`}>
+              className={`text-center text-white text-[28px] font-normal ${prostoOne.className} leading-[30.07px]`}>
               90%
             </div>
           </div>
         </div>
-        <div className="w-full md:basis-[50%] flex flex-col justify-center items-center gap-5">
-          <div
-            className={`h-[42px] flex flex-col justify-center items-start ${lexend.className}`}>
-            <div className="text-zinc-200 text-md md:text-[22px] font-semibold leading-normal">
+        <div className="w-full md:basis-[50%] flex flex-col justify-center  gap-5">
+          <div className={`h-[42px] text-left ${lexend.className}`}>
+            <div className="text-zinc-200 text-md md:text-[22px] font-semibold leading-normal text-left">
               Credit Factors
             </div>
             <div className="text-stone-300 text-xs font-normal leading-[18px]">
@@ -90,7 +89,7 @@ const Dashboard = () => {
             <CreditFactor percentage="30%" label="Wallet Address" />
           </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 };
