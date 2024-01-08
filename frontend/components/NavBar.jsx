cf2f086d4834452a7d9d4b3d0ee36e6cd8df2025
Balaganesh K <k.balaganesh26@gmail.com>
2024-01-08 15:27:27 +0530

    updated the navbar

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ThemeSwitch from '@/components/ThemeSwitch';
+import { Lexend } from 'next/font/google';
+import Link from 'next/link';
+
+const lexend = Lexend({ subsets: ['latin'] });
 
 const NavBar = () => {
   return (
     <div className="w-full h-fit container px-6 md:px-10 mx-auto fixed inset-0 top-5 z-[100]">
       <div className=" flex items-center justify-between px-6 md:px-8 md:py-2 bg-neutral-800 rounded-[40px] h-[72px] ">
-        <div className="text-white text-[40px] font-MXWasgard leading-9">
-          FreedFi
-        </div>
-        <div className="flex items-center gap-6">
-          <div className="text-zinc-200 px-2 py-2 text-sm font-medium font-Lexend leading-normal">
-            About us
+        <Link href={'/'}>
+          <div className="text-white text-[40px] active:scale-90 transition-all duration-200 ease-in-out font-MXWasgard leading-9">
+            FreedFi
           </div>
-          <div className="text-zinc-200 px-2 py-2 text-sm font-medium font-Lexend leading-normal">
-            Documentation
-          </div>
+        </Link>
+        <div className={`flex items-center gap-6 ${lexend.className}`}>
+          <Link
+            href="/marketplace"
+            className="text-zinc-200 px-2 py-2 transition-all duration-200 ease-in-out hover:scale-110 text-base font-medium  leading-normal">
+            Marketplace
+          </Link>
           <ThemeSwitch />
         </div>
       </div>
