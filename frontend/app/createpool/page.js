27f21a2a6abf863b16ae3d5b54945696a5aa2e40
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-01-11 12:46:20 +0530

    Adding Create Pool Page

--- frontend/app/createpool/page.js
+++ frontend/app/createpool/page.js
@@ -54,6 +54,7 @@ const CreatePool = () => {
                 <input
                   class="w-full bg-black mt-2 text-white border border-gray-200 rounded py-3 px-4 mb-3"
                   id="company"
+                  disabled
                   type="text"
                   placeholder="0x12"
                 />
@@ -88,19 +89,19 @@ const CreatePool = () => {
                 />
               </div>
             </div>
-            <div class="-mx-3 md:flex mb-2">
+            <div class="-mx-3 md:flex mb-2 flex-wrap">
               <div class="md:w-1/2 px-3 mb-6 md:mb-0">
                 <label
                   class="uppercase tracking-wide text-white text-xs font-bold mb-2"
                   for="location">
-                  Strategy *
+                  GRANT DISTRIBUTION METHOD *
                 </label>
                 <div>
                   <select
                     class="w-full bg-black mt-2 text-white border text-xs py-3 px-4 pr-8 mb-3 rounded"
                     id="location">
                     <option>Direct</option>
-                    <option>Enugu</option>
+                    <option>Indirect</option>
                     <option>Lagos</option>
                   </select>
                 </div>
@@ -135,6 +136,16 @@ const CreatePool = () => {
                     <option>NFT PLACE</option>
                     <option>DAO's</option>
                   </select>
+                </div>
+              </div>
+              <div class="md:w-1/2 px-3">
+                <label
+                  class="uppercase tracking-wide text-white text-xs font-bold mb-2"
+                  for="department">
+                  Current Date *
+                </label>
+                <div>
+                <input datepicker type="date" className="mt-2 bg-black border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:placeholder-gray-400 dark:text-white " placeholder="Select date"/>
                 </div>
               </div>
             </div>
