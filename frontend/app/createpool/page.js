bb02a30bc244a6eacec4495ea99add642bd50375
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-01-11 12:48:02 +0530

    Adding Create Pool Page

--- frontend/app/createpool/page.js
+++ frontend/app/createpool/page.js
@@ -4,9 +4,9 @@ const CreatePool = () => {
   return (
     <>
       <div className="container mx-auto p-4 px-6 md:px-10 pb-12 flex justify-center items-center flex-col">
-        <h1 className="text-3xl text-center">
-          Create Pool With <br />{' '}
-          <span className="text-blue-400">Aloo Protocol</span>
+        <h1 className="text-3xl text-center mt-8">
+        FreedFi Grant Request powered  <br />{' '}
+          <span className="text-blue-400 mt-3">by ALLO Protocol</span>
         </h1>
         <form className="w-1/2">
           <div class="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
